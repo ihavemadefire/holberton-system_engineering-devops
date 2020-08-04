3a8630e867e1914cd1ca6c5313a0a1a1277dba76
Jacob Ide <jacobmeide@gmail.com>
2020-08-04 05:38:00 +0000

    1

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -1,15 +1,11 @@
 # Sets up config file
 
+
 file_line { 'Turn off passwd auth':
-  ensure => file,
-  path   => '/home/vagrant/.ssh/config',
+  path   => '/etc/ssh/ssh_config',
   line   => 'IdentityFile ~/.ssh/holberton',
-  match  => '^IdentityFile',
 }
 file_line { 'Turn off passwd auth':
-    ensure => file,
-path   => '/home/vagrant/.ssh/config',
-path   => '/home/vagrant/.ssh/config',
-line   => 'PasswordAuthentication no',
-match  => '^PasswordAuthentication',
+  path   => '/etc/ssh/ssh_config',
+  line   => 'PasswordAuthentication no',
 }
