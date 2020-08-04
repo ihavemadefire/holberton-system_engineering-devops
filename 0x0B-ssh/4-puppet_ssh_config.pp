d1fab5b461aa595d721dfad67418089a5beeafda
Jacob Ide <jacobmeide@gmail.com>
2020-08-04 13:55:37 +0000

    0

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -1,12 +1,13 @@
 # Sets up config file
 
-include stdlib
-
 file_line { 'Turn off passwd auth':
+  ensure => 'present',
   path   => '/etc/ssh/ssh_config',
   line   => 'IdentityFile ~/.ssh/holberton',
 }
-file_line { 'Turn off passwd auth':
+
+file_line { 'Declare identity file':
+  ensure => 'present',
   path   => '/etc/ssh/ssh_config',
   line   => 'PasswordAuthentication no',
 }
