48b7123036f5935e519db25dde9218946bb7b7e5
Jacob Ide <jacobmeide@gmail.com>
2020-09-14 21:19:59 +0000

    Change object type from list to dict

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -8,14 +8,14 @@ import requests
 if __name__ == "__main__":
     url_base = "https://jsonplaceholder.typicode.com/"
     names = requests.get(url_base + "users").json()
-    json_dict = [{user.get("id"):
-                  [{"task": i.get("title"),
-                    "completed": i.get("completed"),
-                    "username": user.get("username")}
-                   for i in requests.get(url_base + "todos",
-                                         params={"userId":
-                                                 user.get("id")}).json()]
-                  for user in names}]
+    json_dict = {user.get("id"):
+                 [{"task": i.get("title"),
+                   "completed": i.get("completed"),
+                   "username": user.get("username")}
+                  for i in requests.get(url_base + "todos",
+                                        params={"userId":
+                                                user.get("id")}).json()]
+                 for user in names}
     file_name = 'todo_all_employees.json'
     with open(file_name, 'w') as f:
         json.dump(json_dict, f)
