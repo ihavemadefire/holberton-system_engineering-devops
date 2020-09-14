5118c8432fffd7fc2ba909ac0215f97586c3b398
Jacob Ide <jacobmeide@gmail.com>
2020-09-14 20:22:59 +0000

    Initial commit

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""This module takes in an  """
+
+import json
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    u = sys.argv[1]
+    name = requests.get(
+        "https://jsonplaceholder.typicode.com/users/" + u).json()
+    todos = requests.get(
+        "https://jsonplaceholder.typicode.com/todos/?userId=" + u).json()
+    info = [{"task": i.get("title"),
+              "completed": i.get("completed"),
+              "username": name.get("username")}
+             for i in todos]
+    json_dict = {}
+    json_dict[u] = info
+    file_name = '{}.json'.format(u)
+    with open(file_name, 'w') as f:
+        json.dump(json_dict, f)
