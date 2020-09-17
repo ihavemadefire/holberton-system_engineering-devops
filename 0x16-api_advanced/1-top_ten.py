c83280a2b6d0284bd6e5b0a0af60499e7dea9530
Jacob Ide <jacobmeide@gmail.com>
2020-09-17 19:08:16 +0000

    fix pep8

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -6,10 +6,11 @@ the number of subscribers for that subredit
 
 import requests
 
+
 def top_ten(subreddit):
     headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)\
     AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.102 Safari/537.36"}
-    params={"limit": 10}
+    params = {"limit": 10}
     info = requests.get(
         "http://www.reddit.com/r/{}/hot.json".format(subreddit),
         headers=headers, params=params)
